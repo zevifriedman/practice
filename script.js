4ab7c73a27ccd1325a8655929843b8aef1ce0fdc
MeyronJoseph <126790722+MeyronJoseph@users.noreply.github.com>
2024-06-08 23:44:38 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 const navbar = document.querySelector(".navbar");
 let previousScrollY = 0;
 
-window.addEventListener("scroll", function () {
-  const currentScrollY = window.scrollY;
-  const scrollUp = currentScrollY < previousScrollY;
+// Function to check if the current page is index.html
+function isIndexPage() {
+  return (
+    window.location.pathname === "/index.html" ||
+    window.location.pathname === "/exercises.html"
+  );
+}
 
-  if (scrollUp) {
-    navbar.classList.remove("scrolled");
-  } else {
-    navbar.classList.add("scrolled");
-  }
+// Add scroll event listener conditionally based on the page
+if (!isIndexPage()) {
+  window.addEventListener("scroll", function () {
+    const currentScrollY = window.scrollY;
+    const scrollUp = currentScrollY < previousScrollY;
 
-  previousScrollY = currentScrollY;
-});
+    if (scrollUp) {
+      navbar.classList.remove("scrolled");
+    } else {
+      navbar.classList.add("scrolled");
+    }
+
+    previousScrollY = currentScrollY;
+  });
+}
 
 document.addEventListener("mousemove", function (event) {
   var viewportHeight = window.innerHeight;
@@ -23,47 +34,31 @@ document.addEventListener("mousemove", function (event) {
 
   var buttonHeight = 100; // גובה הכפתורים בפיקסלים
 
-  if (viewportHeight - y <= buttonHeight) {
-    nextButton.style.transition =
-      "opacity 0.5s ease-in-out, transform 0.5s ease-in-out";
-    prevButton.style.transition =
-      "opacity 0.5s ease-in-out, transform 0.5s ease-in-out";
+  if (nextButton) {
+    if (viewportHeight - y <= buttonHeight) {
+      nextButton.style.transition =
+        "opacity 0.5s ease-in-out, transform 0.5s ease-in-out";
 
-    nextButton.style.opacity = "1";
-    prevButton.style.opacity = "1";
+      nextButton.style.opacity = "1";
+      nextButton.style.transform = "translateY(-20px)";
+    } else {
+      nextButton.style.opacity = "0";
+      nextButton.style.transform = "translateY(20px)";
+    }
+  }
 
-    nextButton.style.transform = "translateY(-20px)";
-    prevButton.style.transform = "translateY(-20px)";
-  } else {
-    nextButton.style.opacity = "0";
-    prevButton.style.opacity = "0";
+  if (prevButton) {
+    if (viewportHeight - y <= buttonHeight) {
+      prevButton.style.transition =
+        "opacity 0.5s ease-in-out, transform 0.5s ease-in-out";
 
-    nextButton.style.transform = "translateY(20px)";
-    prevButton.style.transform = "translateY(20px)";
+      prevButton.style.opacity = "1";
+      prevButton.style.transform = "translateY(-20px)";
+    } else {
+      prevButton.style.opacity = "0";
+      prevButton.style.transform = "translateY(20px)";
+    }
   }
 });
 
-function toggleCode(button) {
-  var code = button.previousElementSibling;
-  code.classList.toggle("active");
-  if (code.classList.contains("active")) {
-    button.textContent = "סגור";
-    button.style.transform = "rotate(180deg)";
-    button.style.backgroundColor = "#dc3545";
-    animateCode(code);
-  } else {
-    button.textContent = "הצג הכול";
-    button.style.transform = "rotate(0deg)";
-    button.style.backgroundColor = "#007bff";
-  }
-}
-
-function animateCode(code) {
-  var lines = code.querySelectorAll("p, code");
-  lines.forEach(function (line, index) {
-    setTimeout(function () {
-      line.style.opacity = "1";
-      line.style.transform = "translateX(0)";
-    }, index * 100);
-  });
-}
+//
